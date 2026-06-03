9f54f99bd0e931c2cd8209c594066e71b2286acb
ramyadweb <ramyad.web@gmail.com>
2026-06-04 00:33:50 +0530

    TableChanges

---
+++ src/components/JobCard.jsx
@@ -0,0 +1,20 @@
+function JobCard() {
+    return(
+        <div className="">
+
+            <h3 className="bg-white shadow-md rounded-xl p-6 border border-gray-200">
+                Sarvam</h3>
+
+             <p className="bg-white shadow-md rounded-xl p-6 border border-gray-200">
+                Web Developer </p>
+
+             <p className="bg-white shadow-md rounded-xl p-6 border border-gray-200">
+             
+                Mumbai,India</p>
+
+             <p className="bg-white shadow-md rounded-xl p-6 border border-gray-200">
+                Pending</p>
+        </div>
+    );
+}
+export default JobCard;
--- src/components/RecentApplications.jsx
+++ src/components/RecentApplications.jsx
@@ -1,48 +1,19 @@
+import JobCard from './JobCard';
 function RecentApplications() {
     return(
-        <section className="bg-white py-6 " >
+        <section className="bg-white py-8 " >
             <h2 className="text-center text-2xl py-5 font-bold text-gray-800">
                 Recent Applications
             </h2>
-            <div className="max-w-2xl mx-auto mt-6 ">
-                <table className="w-full border border-gray-300 text-center" >
-                    <thead className="bg-gray-300">
-                      <tr>
-                        <th className="p-3 border border-gray-800">Company</th>
-                        <th className="p-3 border border-gray-800">Role</th>
-                        <th className="p-3 border border-gray-800">Status</th>
-                      </tr>
-                    </thead>
-                    <thead>
-                      <tr>
-                        <td className="p-3 border border-gray-600">Sarvam</td>
-                        <td className="p-3 border border-gray-600">Web Developer</td>
-                        <td className="p-3 border border-gray-600">Pending</td>
-                      </tr>
-                    </thead>
-                    <thead>
-                      <tr>
-                        <td className="p-3 border border-gray-600">ThiDiff</td>
-                        <td className="p-3 border border-gray-600">Software Testing</td>
-                        <td className="p-3 border border-gray-600">Rejected</td>
-                      </tr>
-                    </thead>
-                    <thead>
-                      <tr>
-                        <td className="p-3 border border-gray-600">InSnap</td>
-                        <td className="p-3 border border-gray-600">Data Analyst</td>
-                        <td className="p-3 border border-gray-600">Selected</td>
-                      </tr>
-                    </thead>
-                    <thead>
-                      <tr>
-                        <td className="p-3 border border-gray-600">Synthetics</td>
-                        <td className="p-3 border border-gray-600">Cyber </td>
-                        <td className="p-3 border border-gray-600">Rejected</td>
-                      </tr>
-                    </thead>
-                </table>
+            <div className="gap-6 grid grid-cols-3 mt-8 mx-auto max-w-5xl">
+                <JobCard />
+                <JobCard />
+                <JobCard />
+                
+
             </div>
+            
+            
                    
 
         </section>
